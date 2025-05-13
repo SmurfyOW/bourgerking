42942ab279442c5d2ab6dee321d80ddea40772f0
SmurfyOW <florianwys62@icloud.com>
2025-05-13 16:01:14 +0200

    send v1

---
+++ script.js
@@ -0,0 +1,22 @@
+function changeLocation(city) {
+    let mapIframe;
+    
+    // Remplacer les liens par les Google Maps embed des différents Burger King
+    switch (city) {
+      case 'Liévin':
+        mapIframe = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2541.9455995342178!2d2.7754823768279997!3d50.42348518930474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dd3a721fbbd5b3%3A0xaebe23f0ddebcd90!2sBurger%20King!5e0!3m2!1sfr!2sfr!4v1747054049710!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        break;
+      case 'Béthune':
+        mapIframe = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2536.477009865298!2d2.6242011768327442!3d50.52529718197149!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dd228a874358f7%3A0x28cb17a95bd31f4a!2sBurger%20King!5e0!3m2!1sfr!2sfr!4v1747054273883!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        break;
+      case 'Noeux-les-mines':
+        mapIframe = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10156.433096251985!2d2.6646985871582025!3d50.476327999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dd3d00653f5723%3A0xc38105a96afe7921!2sBurger%20King!5e0!3m2!1sfr!2sfr!4v1747054400595!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        break;
+      default:
+        mapIframe = '<p>Ville non disponible</p>';
+    }
+  
+    // Afficher la carte choisie dans le div
+    document.getElementById('map-display').innerHTML = mapIframe;
+  }
+  
